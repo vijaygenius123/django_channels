1c94fcc4fe55d01eff46da0765dc251b63d94815
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-06-29 22:19:53 +0530

    Tested WS Send & Receive Of Data

--- dashboard/consumers.py
+++ dashboard/consumers.py
@@ -7,8 +7,16 @@ class DashboardConsumer(AsyncWebsocketConsumer):
         await self.channel_layer.group_add(self.groupname,self.channel_name)
         await self.accept()
     
-    async def disconnect(self,close):
+    async def disconnect(self,close_code):
         pass
     
-    async def recieve(self,data):
-        pass
+    async def receive(self,text_data):
+        print("Got: ",text_data)
+        await self.channel_layer.group_send(self.groupname,{
+            'type': 'process',
+            'data': text_data
+        })
+    
+    async def process(self,event):
+        data = event['data']
+        await self.send(text_data=data)
